7d6fa5f5608dbb3d214eaf7f90ee0adbfc584d2d
Andy Dalton <andy.dalton@gmail.com>
2021-12-10 16:19:14 -0500

    Add os adaptation layer
    Directly making calls to os.XXX APIs is difficult to unit test.  We
    wouldn't, for example, want a unit test to be manipulating the
    filesystem.
    
    This change adds a simple adaptation layer over the os APIs.  Clients of
    the os.* function will use this adaptation layer.  The adaptation layer,
    by default, invokes the corresponding os.* function; however, users can
    inject a stub function to call instead.  Unit tests can use that to
    ensure that the expected calls are made to the os.* functions, in the
    expected order, with the expected parameters.

---
+++ pkg/adaptation/os/ostest/mkdirallrecorder.go
@@ -0,0 +1,22 @@
+package ostest
+
+import "github.com/adalton/teleport-exercise/pkg/adaptation/os"
+
+type MkdirAllRecord struct {
+	Path string
+	Perm os.FileMode
+}
+
+type MkdirAllRecorder struct {
+	Events    []*MkdirAllRecord
+	NextError error
+}
+
+func (w *MkdirAllRecorder) MkdirAll(path string, perm os.FileMode) error {
+	w.Events = append(w.Events, &MkdirAllRecord{
+		Path: path,
+		Perm: perm,
+	})
+
+	return w.NextError
+}
