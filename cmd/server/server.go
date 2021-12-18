6013e19dbf9fa0453372a9261090c9e611065a91
Andy Dalton <andy.dalton@gmail.com>
2021-12-18 12:29:39 -0500

    Add flag for address

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -21,13 +21,36 @@ import (
 
 	"github.com/adalton/teleport-exercise/certs"
 	"github.com/adalton/teleport-exercise/pkg/command"
+
+	"github.com/spf13/cobra"
 )
 
-func main() {
+var (
+	argAddress string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "jobmanager",
+	Short: "Run the job manager server",
+	Run: func(cmd *cobra.Command, args []string) {
+		runServer()
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(
+		&argAddress,
+		"address",
+		"a",
+		":24482",
+		"The <address>:<port> on which this server should listen for incoming requests")
+}
+
+func runServer() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	listener, err := net.Listen("tcp", ":24482")
+	listener, err := net.Listen("tcp", argAddress)
 	if err != nil {
 		panic(err)
 	}
@@ -43,4 +66,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	rootCmd.Execute()
 }
